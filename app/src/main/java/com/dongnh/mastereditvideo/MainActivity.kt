80bf10cfb861b2f355f786ab80e8c115c0694d92
dongnh311 <40257252+dongnh311@users.noreply.github.com>
2023-01-05 10:46:04 +0700

    Config to load module.

--- app/src/main/java/com/dongnh/mastereditvideo/MainActivity.kt
+++ app/src/main/java/com/dongnh/mastereditvideo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.dongnh.mastereditvideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dongnh.mastereditvideo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+
 }
